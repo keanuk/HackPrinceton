c75127a42efb960f1f15d100f3a727c803b178e2
keanuk <keanu@akua.com>
2017-11-11 18:09:21 -0500

    Prints to list view

--- app/src/main/java/com/hackprinceton/teamwebscale/hackprinceton/MainActivity.java
+++ app/src/main/java/com/hackprinceton/teamwebscale/hackprinceton/MainActivity.java
@@ -5,7 +5,11 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -16,12 +20,12 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    String JSON_STRING = "{\"transaction\":{\"food\":\"hamburger\",\"calories\":666\",\"date\":test}}";
+    JSONArray fullData;
     String food, calories, date;
-    TextView myFood, myCal, myDate;
+    ListView myFood, myCal, myDate;
+
 
     private TextView mTextMessage;
-
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
@@ -40,33 +44,53 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            fullData=ReadFromFile.readFromFileToJSON(this).getJSONArray("foodData");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
         setContentView(R.layout.activity_main);
 
-        myFood = (TextView) findViewById(R.id.Food);
-        myCal = (TextView) findViewById(R.id.Calories);
-        myDate = (TextView) findViewById(R.id.Date);
+        myFood = (ListView) findViewById(R.id.Food);
+        myCal = (ListView) findViewById(R.id.Calories);
+        myDate = (ListView) findViewById(R.id.Date);
+
+        String[] foodArray = new String[fullData.length()];
+        String[] calArray = new String[fullData.length()];
+        String[] dateArray = new String[fullData.length()];
 
         try {
-            JSONObject obj = new JSONObject(JSON_STRING);
-            JSONObject transaction = obj.getJSONObject("transaction");
-            food = transaction.getString("food");
-            calories = transaction.getString("calories");
-            date = transaction.getString("date");
-            myFood.setText(food);
-            myCal.setText(calories);
-            myDate.setText(date);
-
+            for(int i=0; i<fullData.length(); i++) {
+                foodArray[i] = fullData.getJSONObject(i).getString("foodName");
+                calArray[i] = fullData.getJSONObject(i).getString("calories");
+                dateArray[i] = fullData.getJSONObject(i).getString("date");
+            }
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+        ArrayAdapter<String> foodAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,android.R.id.text1,foodArray);
+        ArrayAdapter<String> calAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,android.R.id.text1,calArray);
+        ArrayAdapter<String> dateAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,android.R.id.text1,dateArray);
+
+        myFood.setAdapter(foodAdapter);
+        myCal.setAdapter(calAdapter);
+        myDate.setAdapter(dateAdapter);
+
+//        try {
+//            food = transaction.getString("food");
+//            calories = transaction.getString("calories");
+//            date = transaction.getString("date");
+//            myFood.setText(food);
+//            myCal.setText(calories);
+//            myDate.setText(date);
+//
+//        } catch (JSONException e) {
+//            e.printStackTrace();
+//        }
 
         mTextMessage = (TextView) findViewById(R.id.message);
-        JSONObject jsonObject=ReadFromFile.readFromFileToJSON(this);
-        try {
-            System.out.println(jsonObject.getJSONArray("foodData"));
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
